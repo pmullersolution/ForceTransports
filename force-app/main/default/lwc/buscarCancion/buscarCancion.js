285bbbe13d4f2e7296bf88ebdb787ee7eedc91b4
Sergio Gregorio Baca Morales <69967192+sf-inetum@users.noreply.github.com>
2022-05-13 21:41:43 +0200

    Buscar scond

--- force-app/main/default/lwc/buscarCancion/buscarCancion.js
+++ force-app/main/default/lwc/buscarCancion/buscarCancion.js
@@ -1,3 +1,21 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class BuscarCancion extends LightningElement {}
+export default class BuscarCancion extends LightningElement {
+    @track artista;
+    @track cancion;
+    @track letra;
+
+    handleArtistaChange(event) {
+        this.artista = event.target.value;
+    }
+
+    handleCancionChange(event) {
+        this.cancion = event.target.value;
+    }
+
+    handleClick(event) {
+        this.letra = this.artista + ' ' + this.cancion;
+        this.template.querySelector(".input").value = this.letra;
+    }
+
+}
