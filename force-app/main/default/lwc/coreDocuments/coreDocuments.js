8490ac431821484a586c6b730e7d3c878a14be23
Sergio Gregorio Baca Morales <69967192+sf-inetum@users.noreply.github.com>
2022-05-16 22:34:20 +0200

    Add Wire Funtions

--- force-app/main/default/lwc/coreDocuments/coreDocuments.js
+++ force-app/main/default/lwc/coreDocuments/coreDocuments.js
@@ -1,19 +1,26 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
 import getDocuments from '@salesforce/apex/coreDocuments.getDocuments';
 
 const columns = [
-    { label: 'Nombre', fieldName: 'Title' },
-    { label: 'Tipo documento', fieldName: 'FileType', type: 'text' },
-    { label: 'Tamaño', fieldName: 'ContentSize', type: 'integer' },
-    { label: 'Creado por', fieldName: 'CreatedBy.Name', type: 'text' }
+    { label: 'Nombre', fieldName: 'name', type: 'text' },
+    { label: 'Tipo documento', fieldName: 'type', type: 'text' },
+    { label: 'Tamaño', fieldName: 'sizeDoc', type: 'integer' },
+    { label: 'Creado por', fieldName: 'createdBy', type: 'text' }
 ];
 
 export default class CoreDocuments extends LightningElement {
     data = [];
     columns = columns;
+    @track errorData;
+    @api recordId;
 
-    async connectedCallback() {
-        const data = await getDocuments();
-        this.data = data;
+    @wire(getDocuments, { idObject: '$recordId' })
+    dataRecord({ data, error }) {
+        if (data) {
+            this.data = data;
+        } else if (error) {
+            this.errorData = error;
+        }
     }
+
 }
